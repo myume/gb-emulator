87ad843e34536a21310973ad01e8207f614efe31
myu <contact@memyu.com>
2025-09-10 01:43:59 -0400

    feat: init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Flake for gameboy emulator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+        ];
+      };
+    });
+}
